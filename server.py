886c5b9307d9ee7c7a11d9f0f7da81c4d434a0de
Nawaf Amjad Rizqi Aldaha Ismail <19623217@std.stei.itb.ac.id>
2024-10-18 15:22:59 +0700

    nambahin pw

--- server.py
+++ server.py
@@ -4,9 +4,15 @@ import queue
 
 messages = queue.Queue()
 clients = []
+user_passwords = {}
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("172.20.10.3", 9999))
+
+valid_passwords = {
+    "aliya": "cantik",
+    "nana": "ganteng",
+}
 
 def receive():
     while True:
@@ -16,17 +22,33 @@ def receive():
         except:
             pass
 
+
 def broadcast():
     while True:
         while not messages.empty():
             message, addr = messages.get()
+            decoded_message = message.decode()
             print(message.decode())
 
             if addr not in clients:
-                clients.append(addr)
-                name = message.decode().split(":", 1)[-1]
-                server.sendto(f"{name} joined!".encode(), addr)
-                
+
+                if addr not in clients: 
+                    if decoded_message.startswith("SIGNUP_TAG:"): 
+                        _, credentials = decoded_message.split("SIGNUP_TAG: ", 1) 
+                        username, password = credentials.split(":")
+                        
+                        if username in valid_passwords and valid_passwords[username] == password: 
+                            clients.append(addr) 
+                            user_passwords[addr] = username 
+                            server.sendto(f"{username} joined!".encode(), addr) 
+                        else: 
+                            server.sendto("Invalid username or password!".encode(), addr) 
+                    continue
+               
+                # clients.append(addr)
+                # name = message.decode().split(":", 1)[-1]
+                # server.sendto(f"{name} joined!".encode(), addr)
+                                
             for client in clients:
 
                 if client != addr:
